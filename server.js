f7673b192e837e69f0397baee5aecddbe6355615
VD057z <drakulavladislav2008@mail.ru>
2025-01-21 21:31:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -18,18 +18,32 @@ app.use(express.static(path.join(__dirname, './')));
 let surveyData = [];
 
 app.post('/send-telegram', async (req, res) => {
-// ...
+    const { name, attending, alcoholMessage, comments } = req.body;
+    surveyData.push({ name, attending, alcoholMessage, comments }); // Добавляем данные в массив
+    res.json({ success: true });
 });
 
 app.post('/download', (req, res) => {
-// ...
+    const password = req.body.password;
+    if (password === '057057') {
+        res.setHeader('Content-disposition', 'attachment; filename=test_data.csv');
+        res.setHeader('Content-type', 'text/csv');
+        res.end('test,test2,test3\n'); // Временная статическая строка
+    } else {
+        res.status(401).send('Неверный пароль');
+    }
 });
 
 app.get('/download', (req, res) => {
-  // ...
+  res.send(`
+      <form method="post" action="/download">
+        <label for="password">Введите пароль:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <button type="submit">Выгрузить данные</button>
+      </form>
+    `);
 });
 
-
 app.listen(port, () => {
-   console.log(`Сервер запущен на порту ${port}`);
+    console.log(`Сервер запущен на порту ${port}`);
 });
